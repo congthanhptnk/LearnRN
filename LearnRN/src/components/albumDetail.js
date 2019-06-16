8d95188032efc3b0b117fe031ae24e1f4e66ee69
Thanh Tran <congthanhptnk@gmail.com>
2019-06-16 16:27:17 +0300

    Add button + card styling

--- LearnRN/src/components/albumDetail.js
+++ LearnRN/src/components/albumDetail.js
@@ -1,16 +1,67 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './card';
 import CardSection from './cardSection';
+import Button from './button';
 
 const AlbumDetail = (props) => {
+  const { title, artist, thumbnail_image, image } = props.album;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={styles.thumbnailContainer}>
+          <Image
+            source={{ uri: thumbnail_image }}
+            style={styles.thumbnailStyle}
+          />
+        </View>
+
+        <View style={styles.headerDetailStyle}>
+          <Text style={styles.titleText}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={styles.imageStyle}
+        />
+      </CardSection>
+
+      <CardSection>
+        <Button />
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerDetailStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  titleText: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainer: {
+    justifyContent: 'center',
+    alignItem: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    //aspectRatio: 1,
+    width: null,
+
+  }
 };
 
 export default AlbumDetail;
