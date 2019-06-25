b77ad9bbd407c137a979ae0fe2a5f326f1685cc3
Thanh Tran <congthanhptnk@gmail.com>
2019-06-25 13:38:54 +0300

    setting up loginform and migrate reusable components from other apps

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
+import firebase from 'firebase';
+
+import { createStore } from 'redux';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyAFpi_Fh1Ln-z5_eOvvIYcxBeGKTm8JkOY',
+      authDomain: 'manager-aa6ff.firebaseapp.com',
+      databaseURL: 'https://manager-aa6ff.firebaseio.com',
+      projectId: 'manager-aa6ff',
+      storageBucket: '',
+      messagingSenderId: '205999694896',
+      appId: '1:205999694896:web:3d02ea1de462ed00'
+  };
+
+    firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
-      <Provider>
-        <View>
-          <Text>HI</Text>
-        </View>
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
       </Provider>
     );
   }
 }
+
+export default App;
---
+++ manager/src/components/LoginForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Card>
+        <Text>HI</Text>
+      </Card>
+    );
+  }
+}
+
+export default LoginForm;
