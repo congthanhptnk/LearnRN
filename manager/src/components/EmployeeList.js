2292753e135dcf8e4c2388c970b2e273040ac729
Thanh Tran <congthanhptnk@gmail.com>
2019-06-26 11:27:42 +0300

    divide scenes into buckets and implement Actions when login success

---
+++ manager/src/components/EmployeeList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>aaaaa</Text>
+        <Text>aaaaa</Text>
+        <Text>aaaaa</Text>
+        <Text>aaaaa</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
