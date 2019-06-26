5cd542bc91bcc4f9be4c554d3bc7c7350769210d
Thanh Tran <congthanhptnk@gmail.com>
2019-06-26 14:14:56 +0300

    fetch live data from firebase

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+
+import { employeeFetch } from '../actions';
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeeFetch();
+  }
+
   render() {
     return (
       <View>
-        <Text>aaaaa</Text>
-        <Text>aaaaa</Text>
-        <Text>aaaaa</Text>
-        <Text>aaaaa</Text>
+        <Text>qweqwe</Text>
       </View>
     );
   }
 }
 
-export default EmployeeList;
+export default connect(null, { employeeFetch })(EmployeeList);
